96ba7a37ac8232bc3e6ff49fff242798ac63f7fc
wesley <wesleyyw8@gmail.com>
2018-10-13 17:21:56 +0100

    index.html removed

--- logic.js
+++ logic.js
@@ -1,5 +1,10 @@
 function nextLoteryDay(dateInserted) {
-  var dateInserted = new Date(dateInserted);
+  var dateInserted;
+  if (!dateInserted) {
+    dateInserted = new Date();
+  } else {
+    dateInserted = new Date(dateInserted);
+  }
   console.log('date sent: ', dateInserted);
   console.log('next lotery day: ', checkDate(dateInserted));
 }
@@ -7,7 +12,10 @@ function checkDate(dateInserted) {
   if (dateInserted.getDay() !== 0  //isnt sunday
     && (dateInserted.getDay() % 3 === 0 || dateInserted.getDay() % 6 === 0) //wednesday or saturday
     && dateInserted.getHours() <= 20) { //before 20:00
-    return new Date(dateInserted.setHours(20));
+    dateInserted.setHours(20);
+    dateInserted.setMinutes(0);
+    dateInserted.setSeconds(0);
+    return new Date(dateInserted);
   }
   var newDate = new Date(dateInserted.setDate(dateInserted.getDate() + 1));
   newDate = new Date(newDate.setHours(20));
@@ -15,3 +23,6 @@ function checkDate(dateInserted) {
 }
 
 nextLoteryDay('August 19, 1975 23:15:30');
+
+
+//nextLoteryDay();
